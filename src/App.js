0211ed03f16268aad51b922cefb0233229de79b1
cladams0203 <cladams0203@gmail.com>
2019-10-04 21:08:52 -0400

    finished form handling for todo add

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
-
+import TodoForm from './components/TodoComponents/TodoForm';
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      list: item
+      list: ''
     }
   }
+  
 
   handleAdd = (input) => {
+  
     const newItem = {
       todo: input,
       id: Date.now(),
@@ -21,7 +23,12 @@ class App extends React.Component {
   handleChange = (id) => {
     this.setState({
       list: this.state.list.map(item => {
-        item.id === id ? {...item, complete: !item.complete} : item
+        if(item.id === id) {
+          return {...item, complete: !item.complete}
+        }else{
+          return item
+        }
+        // item.id === id ? {...item, complete: !item.complete} : item
       })
     })
   }
@@ -33,12 +40,13 @@ class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
-
+  
   render() {
     return (
       <div>
+        {console.log(this.state)}
         <h2>Welcome to your Todo App!</h2>
-        
+        <TodoForm handleAdd={this.handleAdd} />
       </div>
     );
   }
--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -1,24 +1,35 @@
-import React from 'react'
+import React from 'react';
 
 
 class TodoForm extends React.Component {
-    constructor() {
-        super()
-        this.state = ''
+    constructor(props) {
+        super(props)
+        this.state = {
+            value: ''
+        }
     }
     
     render() {
         return (
             <div>
-                <form>
+                <form onSubmit={(e) => {
+                    e.preventDefault()
+                    this.props.handleAdd(this.state.value)
+                    this.setState({value: ''})
+                }}>
                     <input
                     type='text' 
                     name='listItem' 
-                    value={this.itemInput}
+                    value={this.state.value}
+                    onChange={(e) => {
+                        this.setState({value: e.target.value})
+                    }}
                      >
                     </input>
+                    <button type='submit'>Add Todo</button>
                 </form>
             </div>
         )
     }
 }
+export default TodoForm;
